7272ccbd2227085561a619c785c0d72c51feddd4
carlotamartin <91574355+carlotamartin@users.noreply.github.com>
2023-05-17 08:39:34 +0200

    cambios en el reloj

--- js/reloj.js
+++ js/reloj.js
@@ -5,6 +5,17 @@ function actualizarReloj() {
     const minutos = fecha.getMinutes();
     const segundos = fecha.getSeconds();
 
+    //Añadimos un 0 a la izquierda a los números menores de 10
+    if (hora < 10) {
+        hora = "0" + hora;
+    }
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+
     // Formatea la hora como texto
     // Si es necesario, se antepone un 0 a la izquierda para que siempre haya dos dígitos en las horas, minutos y segundos, eso se hace con el método padStart()
     const horaTexto = `${hora.toString()}:${minutos.toString()}:${segundos.toString()}`;
